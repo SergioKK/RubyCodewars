f78bf1e2b9f49dfcafe0a63e4ae848591c2dd621
sergio <khotsenkos1993k@gmail.com>
2024-06-26 19:48:49 +0300

    Solved another kata.

---
+++ RubyPractice2.rb
@@ -0,0 +1,16 @@
+# 11 kata
+
+=begin
+In this simple exercise, you will create a program that will take two lists of integers, a and b. 
+Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. 
+You must find the difference of the cuboids' volumes regardless of which is bigger.
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. 
+Therefore, the function should return 8.
+=end
+
+def find_difference(a, b)
+    (a.inject(:*) - b.reduce(:*)).abs
+end
+
+puts find_difference([3, 2, 5], [1, 4, 4])
+puts find_difference([9, 7, 2], [5, 2, 2])
