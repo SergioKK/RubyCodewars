c971967f132a90f7e44711c1984d640d330f49bf
sergio <khotsenkos1993k@gmail.com>
2024-07-05 15:16:04 +0300

    Solved another kata.

--- RubyPractice4.rb
+++ RubyPractice4.rb
@@ -12,3 +12,20 @@ def capitals(word)
     end
     arr
 end
+
+
+=begin
+Given an array of integers, remove the smallest value. 
+Do not mutate the original array/list. 
+If there are multiple elements with the same value, remove the one with the lowest index. 
+If you get an empty array/list, return an empty array/list.
+Don't change the order of the elements that are left.
+=end
+
+def remove_smallest(numbers)
+    return numbers if numbers.empty?
+    new_numbers = numbers.dup
+    new_numbers.delete_at(new_numbers.index(new_numbers.min))
+    new_numbers
+end
+  
--- test/TestRubyPractice4.rb
+++ test/TestRubyPractice4.rb
@@ -7,4 +7,14 @@ class MyTest < Minitest::Test
         assert_equal(capitals('4ysdf4'), [])
         assert_equal(capitals('ABCDEF'), [0, 1, 2, 3, 4, 5])
     end
+
+    def test_32_kata
+        numbers = [1, 2, 3]
+        remove_smallest(numbers)
+        assert_equal(numbers, [1, 2, 3])
+
+        assert_equal(remove_smallest([1, 2, 3, 4, 5]), [2, 3, 4, 5])
+        assert_equal(remove_smallest([]), [])
+        assert_equal(remove_smallest([2, 2, 1, 2, 1]), [2, 2, 2, 1])
+    end
 end
