7257c539bbf7b505a9bcd570e5dbefa971bf424e
sergio <khotsenkos1993k@gmail.com>
2024-07-09 11:34:33 +0300

    Learn about Array.flatten method.

--- RubyPractice40-49.rb
+++ RubyPractice40-49.rb
@@ -5,3 +5,17 @@
 def add_length(str)
     str.split.map { |word| "#{word} #{word.length}" }
 end
+
+
+# 41 kata
+
+# Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+def flatten_and_sort(array)
+    array.inject { |arr1, arr2| arr1 + arr2 }.sort
+end
+
+# best practice
+def flatten_and_sort(array)
+    array.flatten.sort
+end
--- test/TestRubyPractice40-49.rb
+++ test/TestRubyPractice40-49.rb
@@ -8,4 +8,10 @@ class MyTest < Minitest::Test
         assert_equal(add_length('you'), ["you 3"])
         assert_equal(add_length('y'), ["y 1"])
     end
+
+    def test_41_kata
+        assert_equal(flatten_and_sort([[], [], [], [2], [], [1]]), [1, 2])
+        assert_equal(flatten_and_sort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+        assert_equal(flatten_and_sort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100])
+    end
 end
