84c8fc41afaa99dd6abc9099d1026a032b76e8c6
sergio <khotsenkos1993k@gmail.com>
2024-07-29 12:52:29 +0300

    Solved 76th kata.

--- RubyPractice70-79.rb
+++ RubyPractice70-79.rb
@@ -74,3 +74,23 @@ def revrot(str, sz)
     end
   end.join
 end
+
+
+# 76 kata
+=begin
+There is a queue for the self-checkout tills at the supermarket. 
+Your task is write a function to calculate the total time required for all the customers to check out!
+
+input
+customers: an array of positive integers representing the queue. 
+Each integer represents a customer, and its value is the amount of time they require to check out.
+n: a positive integer, the number of checkout tills.
+output
+The function should return an integer, the total time required.
+=end
+
+def queue_time(customers, n)
+  arr = Array.new(n, 0)
+  customers.each { |customer| arr[arr.index(arr.min)] += customer }
+  arr.max
+end
--- test/TestRubyPractice70-79.rb
+++ test/TestRubyPractice70-79.rb
@@ -51,4 +51,13 @@ class MyTest < Minitest::Test
       assert_equal(revrot("733049910872815764", 5), "330479108928157")
     end
 
+    def test_76_kata
+      assert_equal(queue_time([], 1), 0, "wrong answer for case with an empty queue")
+      assert_equal(queue_time([5], 1), 5, "wrong answer for a single person in the queue")
+      assert_equal(queue_time([2], 5), 2, "wrong answer for a single person in the queue")
+      assert_equal(queue_time([1,2,3,4,5], 1), 15, "wrong answer for a single till")
+      assert_equal(queue_time([1,2,3,4,5], 100), 5, "wrong answer for a case with a large number of tills")
+      assert_equal(queue_time([2,2,3,3,4,4], 2), 9, "wrong answer for a case with two tills")
+    end
+
 end
